0d400449b77ab243872e5b3078c2977793b64769
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-07-25 07:57:21 +0700

    Update VoHub-adm.lua

--- VoHub-adm.lua
+++ VoHub-adm.lua
@@ -31,7 +31,7 @@ getgenv().VO_CONFIG = {
     ExcludeRarities = {},   -- Blacklist rarities (pets on ReleasePets by name bypass this)
 
     -- === AGE PETS ===
-    AgePets = true,
+    AgePets = false,
     AgePetsNames = {"Violet Friend", "Sunflower Friend"},
     AgePetsTypes = {"All"},  -- "Normal", "Neon", "ALL"
 
